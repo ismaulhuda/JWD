647d947fd048b3a9cf07aa1da4b3f0cc405ae43c
ismaulhuda <71919796+ismaulhuda@users.noreply.github.com>
2023-08-23 14:01:55 +0700

    Add files via upload

---
+++ Projek_Beasiswa_jwd/index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <?php include 'koneksi.php' ?>
+        <?php include 'header.php' ?>
+        <link rel="stylesheet" href="style/style_index.css">
+    </head>
+    <body>
+        <div class="main-container">
+            <div class="content">
+                <nav class="navbar navbar-expand-lg">
+                    <?php include 'navbar.php' ?>
+                </nav>
+                <!-- Content Wrapper. Contains page content -->
+                <div class="content-wrapper">
+                    <div class="container">
+                        <div class="content">
+                            <div class="judul">
+                                <h1><b>Selamat Datang Calon Peserta Digital Talent</b></h1>
+                            </div>
+                            <p>Silahkan pilih <b>Menu Daftar Baru</b> untuk melakukan pendaftaran</p>
+                        <!-- Jenis Pilihan Beasiswa dan Ketentuan Syarat -->
+                        <div class="beasiswa">
+                            <h3>Jenis Pilihan Beasiswa:</h3>
+                            <button class="btn btn-primary" onclick="showBeasiswaInfo('akademik')">Beasiswa Akademik</button>
+                            <button class="btn btn-primary" onclick="showBeasiswaInfo('non-akademik')">Beasiswa Non Akademik</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>       
+            </div>
+</div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script>
+            function showBeasiswaInfo(beasiswaType) {
+                if (beasiswaType === 'akademik') {
+                    window.open('daftar_tambah.php');
+                } else if (beasiswaType === 'non-akademik') {
+                    window.open('beasiswa_non_akademik.html', '_blank');
+                }
+            }
+        </script>
+    </body>
+</html>
